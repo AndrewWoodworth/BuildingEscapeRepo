ddd7f5bf700af59c544c4bd7ac8764130d6f6e3a
Andrew Woodworth <manlydrew2@gmail.com>
2019-12-20 07:38:46 -0600

    Made the door's yaw printed out to the log only go to the second decimal (hundreths place)

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -33,7 +33,7 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	UE_LOG(LogTemp, Warning, TEXT("Object Yaw is: %s"), CurrentYaw);
+	UE_LOG(LogTemp, Warning, TEXT("Object Yaw is: %s"), *FString::Printf(TEXT("%.2f"), CurrentYaw));
 
 	DoorRotation.Yaw = FMath::Lerp(CurrentYaw, TargetYaw, 1.f * DeltaTime);
 	CurrentYaw = DoorRotation.Yaw;
